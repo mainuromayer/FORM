1c737705a014c4cb570ae556909aa17189fabcaa
mainuromayer <mainuromayer@gmail.com>
2023-05-07 12:48:49 +0600

    FORM - checkbox (single/multiple check) function

--- assets/form_checkbox-function.php
+++ assets/form_checkbox-function.php
@@ -6,7 +6,6 @@ function isGameChecked($inputName, $value){
     }
 }
 
-
 function isFruitChecked($value){
     if(isset($_REQUEST['fruits']) && is_array($_REQUEST['fruits']) && in_array($value, $_REQUEST['fruits'])){
         echo ' checked ';
